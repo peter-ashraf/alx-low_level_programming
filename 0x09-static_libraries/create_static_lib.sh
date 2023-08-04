c447bcd5cc8f9f76e46cd65001e5c12e85e168f3
Peter Ashraf <peter.ashraf.gendy@gmail.com>
2023-08-04 19:38:14 +0300

    task1, and 2

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Compile each .c file into object files
 gcc -c *.c
-
-# Create the static library liball.a from all the object files
-ar rcs liball.a *.o
-
-# Clean up by removing the object files
-rm -f *.o
-
-echo "Static library liball.a created successfully."
-
+ar -rcs liball.a *.o
