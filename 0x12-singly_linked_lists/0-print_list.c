d7a7624fde99c533569bae571ed5bc005ad05a74
Peter Ashraf <peter.ashraf.gendy@gmail.com>
2023-08-25 00:22:31 +0300

    all

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,16 +3,16 @@
 
 /**
  * print_list - Prints all the elements of a list_t list.
- * @h: A pointer to the head of the linked list.
+ * @h: the list_t list.
  *
- * Return: The number of nodes in the linked list.
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
