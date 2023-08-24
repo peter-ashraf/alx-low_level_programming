88326f6df0bc7c6f9f72b17982a576b6b26cadb2
Peter Ashraf <peter.ashraf.gendy@gmail.com>
2023-08-25 00:25:27 +0300

    all

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,26 +10,31 @@
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new element.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = (list_t *)malloc(sizeof(list_t));
-	
-	if (new_node == NULL)
+	list_t *new;
+	char *dup_str;
+	int len;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup_str = strdup(str);
+	if (dup_str == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	for (len = 0; str[len];)
+		len++;
 
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+	new->str = dup_str;
+	new->len = len;
+	new->next = *head;
 
-	return (new_node);
+	*head = new;
+
+	return (new);
 }
